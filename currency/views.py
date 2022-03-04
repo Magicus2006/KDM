d7ea3a10340d4574e5b2b1248142e48fc20e9a7a
Magicus2006 <61295687+Magicus2006@users.noreply.github.com>
2022-03-05 01:20:24 +0300

    Add currency

--- currency/views.py
+++ currency/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import requests
-from requests.structures import CaseInsensitiveDict
+from datetime import date
+#from requests.structures import CaseInsensitiveDict
 import xml.etree.ElementTree as ET
 
 
@@ -11,18 +12,32 @@ import xml.etree.ElementTree as ET
 
 class CurrencyAPI(APIView):
     def get(self, request):
-        url = "https://cbr.ru/scripts/XML_daily.asp?date-req=04/03/2022"
+        url = "https://cbr.ru/scripts/XML_daily.asp?date-req=05/03/2022"
         result = requests.get(url=url)
 
-        #print(result.content)
-        root = ET.fromstring(result.content)
-        print(root.tag, root.attrib)
-        json_respon = {}
-        for childs in root:
-            t = {}
-            for child in childs:
-                t.update({child.tag: child.text})
-            json_respon.update({childs.attrib["ID"]: t})
+        date_req = self.request.query_params.get('date-req')
+        if date_req == None:
+            t = date.today()
+            date_now = t.strftime("%d/%m/%Y")
+        else:
+            date_now = date_req
 
-        print(json_respon)
+        print(result.content)
+        json_respon = {}
+
+        try:
+            root = ET.fromstring(result.content)
+            print(root.tag, root.attrib)
+
+            for childs in root:
+                t = {}
+                for child in childs:
+                    t.update({child.tag: child.text})
+                json_respon.update({childs.attrib["ID"]: t})
+
+            print(json_respon)
+        except ET.ParseError as e:
+            print(e)
+            return Response(f'Error: {e}, {e.code} {e.position}')
+
         return Response(json_respon)
